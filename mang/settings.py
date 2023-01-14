8f7402e11ccda676ed55eb810a7a668b8266aef3
Kofi532 <56702732+Kofi532@users.noreply.github.com>
2023-01-14 21:49:56 +0000

    Add files via upload

--- mang/settings.py
+++ mang/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-24%8e+u$8#104@0fxj^uon+lehxm6xm_8xde02jrf1gduai9$u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
