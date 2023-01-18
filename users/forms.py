e8fe69bf199b9963719eda5ae8b99935acde213e
Kofi532 <56702732+Kofi532@users.noreply.github.com>
2023-01-18 15:24:37 +0000

    Add files via upload

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 from .models import use, sch_reg, act, class_fee, classn
+from django.contrib.auth.models import User
 
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username','email','password']
 
 class PostForm(forms.ModelForm):
 
