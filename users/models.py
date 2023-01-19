1cbd96f9409cdb8b63cb9b02d2a5412c00da077d
Kofi532 <56702732+Kofi532@users.noreply.github.com>
2023-01-19 21:56:18 +0000

    Add files via upload

--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from datetime import date
-
 # Create your models here.
 class use(models.Model):       
     username = models.CharField(max_length=10, default='')
@@ -16,8 +15,8 @@ class sch_reg(models.Model):
     username = models.CharField(max_length=10, default='')
     school = models.CharField(max_length=150,null=True)
     date = models.DateField(default=date.today())
-    full_sch = models.CharField(max_length=30, default='0')
-    contact_details = models.CharField(max_length=10, default='0')
+    full_sch = models.CharField(max_length=50, default='0')
+    contact_details = models.CharField(max_length=25, default='0')
     def __str__(self):
         return self.username
 
@@ -79,3 +78,4 @@ class classn(models.Model):
 
     def __str__(self):
         return self.classA
+
